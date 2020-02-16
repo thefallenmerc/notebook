7691599d339eee975e5c2524d737dbcae585833e
Shubham Singh Chahar <thefallenmerc@gmail.com>
2020-02-17 01:15:32 +0530

    added mobile responsiveness

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,6 +6,7 @@ const Sidebar = ({ noteList }) => {
 
     const [query, setQuery] = useState('');
     const [notes, setNotes] = useState(noteList);
+    const [mobileSidebarVisibility, setMobileSidebarVisibility] = useState(false);
 
     useEffect(_ => {
         setNotes(noteList);
@@ -30,21 +31,22 @@ const Sidebar = ({ noteList }) => {
     }
 
     return (
-        <section className="sideBar">
+        <section className={"sideBar" + (mobileSidebarVisibility ? "" : " not-visible")}>
             <div className="logo font-bold p-3 text-xl uppercase">
-                <Link to="/" className="cursor-pointer text-red-500">Notebook</Link>
-                <Link to="/new" className="float-right uppercase rounded text-white bg-red-500 hover:bg-red-600 text-sm py-1 px-2 cursor-pointer">New</Link>
+                <Link to="/" onClick={() => { setMobileSidebarVisibility(false) }} className="cursor-pointer text-red-500">Notebook</Link>
+                <span onClick={() => { setMobileSidebarVisibility(!mobileSidebarVisibility) }} className="md:hidden block float-right px-3 ml-2 rounded text-white bg-red-500 hover:bg-red-600 text-sm py-1 px-2 cursor-pointer">&#8942;</span>
+                <Link to="/new" onClick={() => { setMobileSidebarVisibility(false) }} className="float-right rounded text-white bg-red-500 hover:bg-red-600 text-sm py-1 px-2 cursor-pointer">New</Link>
             </div>
             <input
                 placeholder="Search"
-                className=" border-red-300 border-t border-b border-r-0 border-l-0 focus:outline-none"
+                className="md:block hidden border-red-300 border-t border-b border-r-0 border-l-0 focus:outline-none"
                 onChange={search}
                 onKeyUp={clearIfEscape}
                 value={query} />
             <div className="py-2 sidebarLinkContainer">
                 {
                     notes.map(note => {
-                        return <Link to={"/note/" + note.id} key={note.id} className="p-2 hover:bg-red-500 hover:text-white cursor-pointer block">{note.title}</Link>;
+                        return <Link to={"/note/" + note.id} key={note.id} onClick={() => { setMobileSidebarVisibility(false) }} className="p-2 hover:bg-red-500 hover:text-white cursor-pointer block">{note.title}</Link>;
                     })
                 }
             </div>
