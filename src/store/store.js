808c27a6d07f6239b5616d052004c6f60083a8bf
Shubham Singh Chahar <thefallenmerc@gmail.com>
2020-03-29 13:23:54 +0530

    removed logger and devtools extension from production

--- src/store/store.js
+++ src/store/store.js
@@ -5,9 +5,14 @@ import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
 
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const middlewares = thunk;
-const store = createStore(rootReducer, {}, composeEnhancers(applyMiddleware(middlewares, logger)));
+let composeEnhancers;
+const middlewares = [thunk];
+if(process.env.NODE_ENV === 'development') {
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    middlewares.push(logger);
+} else {
+    composeEnhancers = compose;
+}
+const store = createStore(rootReducer, {}, composeEnhancers(applyMiddleware(...middlewares)));
 
 export default store;
